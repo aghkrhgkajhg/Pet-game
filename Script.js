3818a2c39983eed1e9dade3c67d9f3d2ac98de7e
aghkrhgkajhg <014533@abpat.qld.edu.au>
2026-06-13 21:21:19 +1000

    Add loyalty feature and refactor level up logic

--- Script.js
+++ Script.js
@@ -1,14 +1,16 @@
 let level = 1;
 let xp = 0;
 let happiness = 100;
+let loyalty = 0;
 
 function updateUI() {
   document.getElementById("level").innerText = level;
   document.getElementById("xp").innerText = xp;
   document.getElementById("happiness").innerText = happiness;
+  document.getElementById("loyalty").innerText = loyalty;
 
-  let xpNeeded = level * 100;
-  let percent = (xp / xpNeeded) * 100;
+  let needed = level * 100;
+  let percent = (xp / needed) * 100;
 
   document.getElementById("xpBar").style.width = percent + "%";
 }
@@ -16,22 +18,32 @@ function updateUI() {
 function feedPet() {
   xp += 20;
   happiness += 5;
+  loyalty += 1;
 
-  checkLevelUp();
+  if (happiness > 100) happiness = 100;
+
+  levelUp();
   updateUI();
 }
 
 function playPet() {
   happiness += 10;
+  loyalty += 2;
+
+  if (happiness > 100) happiness = 100;
+
   updateUI();
 }
 
-function checkLevelUp() {
+function levelUp() {
   let needed = level * 100;
 
   if (xp >= needed) {
     level++;
     xp = 0;
+
+    happiness += 10;
+    loyalty += 5;
   }
 }
 
